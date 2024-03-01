fead561454758a8a8478354b426b3bea41f769c0
Joel <161714040+omacdonaldhadafarm@users.noreply.github.com>
2024-03-01 22:15:00 +0800

    Update Insurance.java

--- Source Codes/Insurance.java
+++ Source Codes/Insurance.java
@@ -1,19 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Insurance {
-    private double insuranceBalance = 0;
-    private String insuranceType, insurancePlan, policyName;
-    private int policyNumber;
+    private double premiumBalance = 0; //To pay monthly for insurance
+    private double coverageBalance = 0; //To claim when i got into an accident :<
+    private Date startDate;
+
+    private Date endDate;
+    private String insuranceType, policyName, policyNumber;
 
     // Ask what does add insurance means:
-    public void addInsurance(double amount){
-        insuranceBalance += amount;
+    public Insurance(String insuranceType, String policyName, String policyNumber,
+                     double premiumBalance, double coverageBalance, Date startDate,
+                     Date endDate){
+        this.insuranceType = insuranceType;
+        this.policyName = policyName;
+        this.policyNumber = policyNumber;
+        this.premiumBalance = premiumBalance;
+        this.coverageBalance = coverageBalance;
+        this.startDate = startDate;
+        this.endDate = endDate;
+
+    }
+    public void payOffPremium(double amt){
+        premiumBalance += amt;
+        System.out.println("This amount of " + amt + " has been paid!");
+        System.out.println("This is the current premium balance: " + premiumBalance);
     }
 
     public double getInsuranceBalance(){
-        return insuranceBalance;
-    }
-
-    public String getInsurancePlan(){
-        return insurancePlan;
+        return premiumBalance;
     }
 
     public String getInsuranceType(){
@@ -24,16 +44,12 @@ public class Insurance {
         return policyName;
     }
 
-    public int getPolicyNumber(){
+    public String getPolicyNumber(){
         return policyNumber;
     }
 
-    public void setInsuranceBalance(double balance){
-        this.insuranceBalance = balance;
-    }
-
-    public void setInsurancePlan(String plan){
-        this.insurancePlan = plan;
+    public void setPremiumBalance(double balance){
+        this.premiumBalance = balance;
     }
 
     public void setInsuranceType(String type){
@@ -44,14 +60,13 @@ public class Insurance {
         this.policyName = name;
     }
 
-    public void setPolicyNo(int number){
+    public void setPolicyNo(String number){
         this.policyNumber = number;
     }
 
     public void printInsuranceDetail(){
-        System.out.println("Insurance Balance: " + insuranceBalance
+        System.out.println("Insurance Balance: " + premiumBalance
                         + "\nInsurance Type: " + insuranceType
-                        + "\nInsurance Plan: " + insurancePlan
                         + "\nPolicy Name: " + policyName
                         + "\nPolicy Number: " + policyNumber);
     }
