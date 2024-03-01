c9a394a0b6a0ba676897a023f361d3006ba23786
Joel <161714040+omacdonaldhadafarm@users.noreply.github.com>
2024-03-01 22:15:50 +0800

    Update Insurance.java

--- Source Codes/Insurance.java
+++ Source Codes/Insurance.java
@@ -1,9 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Insurance {
     private double premiumBalance = 0; //To pay monthly for insurance
