a27176be40464895506b652db1831d9fd8278fc7
kwanny2301 <161841975+kwanny2301@users.noreply.github.com>
2024-03-03 00:32:11 +0800

    update

--- Source Codes/CreditCard.java
+++ Source Codes/CreditCard.java
@@ -1,15 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Credit_Card {
+public class CreditCard {
     private double balance = 0, creditLimit;
     private String cardHolderName, accountID;
     private String cardNumber;
     private LocalDate expiryDate;
     private List<String> transactionHistory;
+    private double annualPercentageRate;
+    private ArrayList<ArrayList<String>> creditCardDetails;
 
-    public Credit_Card(String cardHolderName, String accountID, String cardNumber, double creditLimit){
+    //Constructors
+    public CreditCard(String cardHolderName, String accountID, String cardNumber, double creditLimit){
         this.cardHolderName = cardHolderName;
         this.accountID = accountID;
         this.cardNumber = cardNumber;
@@ -17,7 +23,52 @@ public class Credit_Card {
         this.expiryDate = currentDate.plusYears(5);  // Expiry set to current date + 5 years
         this.creditLimit = creditLimit;
         this.transactionHistory = new ArrayList<>();
+        this.annualPercentageRate = 20;
     }
+
+    public CreditCard(String filePath) {
+        creditCardDetails = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            boolean firstLine = true; // Flag to skip the first line
+            while ((line = reader.readLine()) != null) {
+                if (firstLine) {
+                    firstLine = false;
+                    continue; // Skip the first line
+                }
+                String[] parts = line.split(","); // Split by comma since it's CSV
+                ArrayList<String> creditCard = new ArrayList<>();
+                for (String part : parts) {
+                    creditCard.add(part.trim());
+                }
+                creditCardDetails.add(creditCard);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        CreditCard creditCard = new CreditCard("src/Project/CreditCard.csv");
+        creditCard.printCreditCardDetails();
+
+    }
+
+    //Methods
+    public void applyMonthlyInterest(){
+        //interest is compounded monthly but calculated daily
+        int daysInMonth = LocalDate.now().lengthOfMonth();
+        double dailyRate = annualPercentageRate/365/100;
+        double interestForMonth = balance * dailyRate * daysInMonth;
+
+        //charge the monthly interest to the balance
+        balance += interestForMonth;
+        transactionHistory.add("[" + LocalDate.now() + "] Interest of " + interestForMonth + " applied to balance.");
+
+        }
+    
 
     public void chargeCredit(double amount){
         if (LocalDate.now().isAfter(expiryDate)){
