44370a8ee5169b892b5d66bc040fcf5d7b688984
BoonYeew <147385740+BoonYeew@users.noreply.github.com>
2024-03-03 01:02:43 +0800

    Update Customer.java

--- Source Codes/Customer.java
+++ Source Codes/Customer.java
@@ -1,16 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Date;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.ZoneId;
+
 
 public class Customer {
 
-    private int age, contactNo;
-    private String address, id, customerName, maritalStatus, country, emailAddress, occupation, employer;
-    private Date dateOfBirth;
+    private int Age, ContactNo;
+    private String Address, ID, CustomerName, MaritalStatus, Country, EmailAddress, Occupation, Employer, DateOfBirth;
     
 
     /* public Customer(String customerName, String id, int age,int ContactNo, String address, String MaritalStatus, String Country, 
@@ -28,29 +24,26 @@ public class Customer {
         this.dateOfBirth = dateOfBirth;
     } */
 
-    public Customer(int customerID){
+    public Customer(String customerID){
         String Path = "Source_Codes/Customer.csv";
   
         try (BufferedReader br = new BufferedReader(new FileReader(Path))) {
             String line = "";
             String delimiter = ",";
-            DateTimeFormatter format = DateTimeFormatter.ofPattern("ddmmyyyy"); 
             
               while ((line = br.readLine()) != null) {
                 String[] data = line.split(delimiter);
-                String id = data[0];
-                this.customerName = data[1];
-                this.age = Integer.parseInt(data[2]);
-                this.contactNo = Integer.parseInt(data[3]);
-                this.address = data[4];
-                this.maritalStatus = data[5];
-                this.country = data[6];
-                this.emailAddress = data[7];
-                this.occupation = data[8];
-                this.employer = data[9];
-                LocalDate localDate = LocalDate.parse(data[10], format);
-                this.dateOfBirth = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-                //Date dateOfBirth =  new Date(data[10]);
+                this.ID = data[0];
+                this.CustomerName = data[1];
+                this.Age = Integer.parseInt(data[2]);
+                this.ContactNo = Integer.parseInt(data[3]);
+                this.Address = data[4];
+                this.MaritalStatus = data[5];
+                this.Country = data[6];
+                this.EmailAddress = data[7];
+                this.Occupation = data[8];
+                this.Employer = data[9];
+                this.DateOfBirth = data[10];
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -60,90 +53,90 @@ public class Customer {
 
 
     public int getContactNo(){
-        return contactNo;
+        return ContactNo;
     }
 
     public String getMaritalStatus(){
-        return maritalStatus;
+        return MaritalStatus;
     }
 
     public String getCountry(){
-        return country;
+        return Country;
     }
     public String getEmailAddress(){
-        return emailAddress;
+        return EmailAddress;
     }
     public String getOccupation(){
-        return occupation;
+        return Occupation;
     }
     public String getEmployer(){
-        return employer;
+        return Employer;
     }
     public String getCustomerName() {
-        return customerName;
+        return CustomerName;
     }
     public int getCustomerAge(){
-        return age;
+        return Age;
     }
 
     public String getCustomerAddress(){
-        return address;
+        return Address;
     }
 
-    public Date getCustomerDOB(){
-        return dateOfBirth;
+    public String getCustomerDOB(){
+        return DateOfBirth;
     }
 
     public String getCustomerNRIC(){
-        return id;
+        return ID;
     }
     public void setContactNo(int ContactNo){
-        this.contactNo = ContactNo;
+        this.ContactNo = ContactNo;
     }
     public void setMaritalStatus(String maritalStatus){
-        this.maritalStatus = maritalStatus;
+        this.MaritalStatus = maritalStatus;
     }
     public void setCountry(String Country){
-        this.country = Country;
+        this.Country = Country;
     }
     public void setEmailAddress(String emailAddress){
-        this.emailAddress = emailAddress;
+        this.EmailAddress = emailAddress;
     }
     public void setOccupation(String occupation){
-        this.occupation = occupation;
+        this.Occupation = occupation;
     }
     public void setEmployer(String employer){
-        this.employer = employer;
+        this.Employer = employer;
     }
     public void setCustomerAge(int age){
-        this.age = age;
+        this.Age = age;
     }
 
     public void setCustomerAddress(String address){
-        this.address = address;
+        this.Address = address;
     }
-    public void setCustomerDOB(Date dob){
-        this.dateOfBirth = dob;
+    public void setCustomerDOB(String dob){
+        this.DateOfBirth = dob;
     }
     public void setCustomerName(String name){
-        this.customerName = name;
+        this.CustomerName = name;
     }
     public void setCustomerNRIC(String id){
-        this.id = id;
+        this.ID = id;
     }
 
     public void printCustomerDetails(){
-        System.out.println("Name: " + customerName
-                + "\nAge: " + age
-                + "\nAddress: " + address
-                + "\nDate of Birth: " + dateOfBirth
-                + "\nID: " + id
-                + "\nContact Number: " + contactNo
-                + "\nMarital Status: " + maritalStatus
-                + "\nCountry: " + country
-                + "\nEmail Address: " + emailAddress
-                + "\nOccupation: " + occupation
-                + "\nEmployer: " + employer);
+        System.out.println("Name: " + CustomerName
+                + "\nAge: " + Age
+                + "\nAddress: " + Address
+                + "\nDate of Birth: " + DateOfBirth
+                + "\nID: " + ID
+                + "\nContact Number: " + ContactNo
+                + "\nMarital Status: " + MaritalStatus
+                + "\nCountry: " + Country
+                + "\nEmail Address: " + EmailAddress
+                + "\nOccupation: " + Occupation
+                + "\nEmployer: " + Employer);
     }
 }
 
