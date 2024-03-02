3bf8b91a411a69f5b92b55fb27db155fbbdf5a3c
kwanny2301 <161841975+kwanny2301@users.noreply.github.com>
2024-03-02 19:57:48 +0800

    updated

--- Source Codes/Credit_Card.java
+++ Source Codes/Credit_Card.java
@@ -1,21 +1,56 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Credit_Card {
     private double balance = 0, creditLimit;
-    private String cardHolderName, id;
-    private int cardNumber;
+    private String cardHolderName, accountID;
+    private String cardNumber;
     private LocalDate expiryDate;
+    private List<String> transactionHistory;
 
-    public Credit_Card(String cardHolderName, String id, int cardNumber){
+    public Credit_Card(String cardHolderName, String accountID, String cardNumber, double creditLimit){
         this.cardHolderName = cardHolderName;
-        this.id = id;
+        this.accountID = accountID;
         this.cardNumber = cardNumber;
         LocalDate currentDate = LocalDate.now(); // LocalDate is to retrieve year-month-day
         expiryDate = currentDate.plusYears(5);  // Expiry set to current date + 5 years
+        this.creditLimit = creditLimit;
+        this.transactionHistory = new ArrayList<>();
     }
 
     public void chargeCredit(double amount){
-        balance += amount;
+        if (LocalDate.now().isAfter(expiryDate)){
+            System.out.println("Transaction declined. The credit card is expired.");
+        }else if (amount <=0){
+            System.out.println("Invalid purchase amount.");
+        }else if (amount > availableCredit()){
+            System.out.println("Purchase exceeds credit limit.");
+        }else {
+            balance += amount;
+            System.out.println("Purchase of " + amount + " made successfully!");
+            transactionHistory.add("[" + LocalDate.now() + "] Charge of " + amount + " added to balance");
+        }
+    }
+
+    public void payBill(double amount){
+        if (amount <=0 || amount > balance){
+            System.out.println("Invalid payment amount.");
+        } else {
+            balance -= amount;
+            System.out.println("Credit card bill of " + amount + " made successfully");
+            transactionHistory.add("[" + LocalDate.now() + "] Payment of " + amount + " deducted from balance");
+        }
+    }
+    public double availableCredit(){
+        return creditLimit - balance;
+    }
+
+    public void printTransactionHistory(){
+        System.out.println("Transaction History:");
+        for (String transaction : transactionHistory){
+            System.out.println(transaction);
+        }
     }
 
     public double getCreditBalance(){
@@ -34,16 +69,12 @@ public class Credit_Card {
         return creditLimit;
     }
 
-    public int getCardNumber(){
+    public String getCardNumber(){
         return cardNumber;
     }
 
-    public String getID(){
-        return id;
-    }
-
-    public void setCreditBalance(double balance){
-        this.balance = balance;
+    public String getAccountID(){
+        return accountID;
     }
 
     public void setCardExpiry(LocalDate date){
@@ -58,12 +89,12 @@ public class Credit_Card {
         this.creditLimit = limit;
     }
 
-    public void setCardNumber(int number){
+    public void setCardNumber(String number){
         this.cardNumber = number;
     }
 
-    public void setID(String id){
-        this.id = id;
+    public void setAccountID(String id){
+        this.accountID = id;
     }
 
     public void printCreditCardDetails(){
@@ -72,6 +103,6 @@ public class Credit_Card {
                 + "\nCard Number: " + cardNumber
                 + "\nCredit Limit: " + creditLimit
                 + "\nExpiry Date: " + expiryDate
-                + "\nID: " + id);
+                + "\nAccount ID: " + accountID);
     }
 }
