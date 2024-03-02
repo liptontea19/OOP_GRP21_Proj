6255964e8478343277fe6eef27aad8455faa235d
liptontea19 <daniel.lipws@gmail.com>
2024-03-02 17:19:13 +0800

    Changed Account constructor, created account.csv

--- Source Codes/Account.java
+++ Source Codes/Account.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Date;
 
 public class Account {
@@ -10,32 +13,59 @@ public class Account {
     private Customer customer;
     private Insurance insurance;
 
-    public Account(Branch branch, int accountNumber, String accountType,
-                   Customer customer, double balance,
-                   double transferLimit){
-        this.branch = branch;
+    /*public Account(int accountNumber, String accountType, int branchCode, 
+                    double balance, Customer customer, double transferLimit){
+        //this.branch = branch;
         this.accountNumber = accountNumber;
         this.accountType = accountType;
         this.customer = customer;
         this.balance = balance;
         this.transferLimit = transferLimit;
+
+        //Branch this.branch = new Branch(branchCode);
+    }*/
+
+    public Account(int accountNumber){
+        String filePath = "Source_Codes/Account.csv";
+        //Fetches account details from Account.csv using accountNumber and initialises into class attribute
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));   // Instantiates bufferedReader obj to read Account CSV file
+            String line;
+            String[] accDetail;
+            while ((line = reader.readLine()) != null){
+                accDetail = line.split(",");
+                if (accDetail[0] == Integer.toString(accountNumber)) {
+                    this.accountType = accDetail[1];
+                    this.balance = Double.parseDouble(accDetail[2]);
+                    //this.branch = new Branch(accDetail[3]);
+                    //this.Customer = new Customer(accDetail[4])// instantiates a Customer obj using the specified customer ID
+                    this.transferLimit = Double.parseDouble(accDetail[5]);
+                }
+            }
+            reader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Unable to locate account ID: " + Integer.toString(accountNumber));
+        }
     }
 
-    public Account(Insurance insurance, Credit_Card creditCard){
+    /*public Account(Insurance insurance, Credit_Card creditCard){
         this.insurance = insurance;
         this.creditCard = creditCard;
-    }
+    }*/
 
     public void addInsurance(String insuranceType, String policyName, String policyNumber,
                              double premiumBalance, double coverageBalance, Date startDate,
                              Date endDate){
-        Insurance newInsurance = new Insurance(insuranceType, policyName, policyNumber,
-                premiumBalance, coverageBalance, startDate, endDate);
+        //Insurance newInsurance = new Insurance(insuranceType, policyName, policyNumber,
+                //premiumBalance, coverageBalance, startDate, endDate);
     }
 
-    public float calculateInterest(){
-        // Calculate Interest Function
-        return interestRate;
+    public double calculateInterest(){
+        // Calculate Interest per annum 
+        double interest = balance * interestRate;
+
+        return interest;
     }
 
     public double checkBalance(){
@@ -49,8 +79,8 @@ public class Account {
     public void deposit(double amount){
         // deposit function
         balance += amount;
-        System.out.println("Amount of " + amount + " has been deposited into " + accountNumber);
-        System.out.println("Current Balance: " + balance);
+        System.out.println("Amount of $" + amount + " has been deposited into " + accountNumber);
+        System.out.println("Current Balance: $" + balance);
 
     }
 
@@ -69,44 +99,46 @@ public class Account {
     public void insuranceDeposit(Insurance insurance){
         // Function to pay for insurance premium
         double insuranceBalance = insurance.getInsuranceBalance();
-        System.out.println("Account " + accountNumber + " has an insurance premium balance of $" + insuranceBalance + " outstanding.");
-        System.out.println("Input the amount to pay from account:");
-
-
-
-
+        System.out.println("Account " + accountNumber + " has an insurance premium balance of $" + insuranceBalance + " outstanding for " + insurance.getPolicyName() + " policy.");
+        if (balance > insuranceBalance){
+            balance -= insuranceBalance;
+            insurance.payOffPremium(insuranceBalance);
+        }
+        else {
+            System.out.println("There is insufficient balance in account.");
+            System.out.println("Current Account Balance: $" + balance);
+        }
     }
 
     public void makePayment(double amount){
-        // Make payment for credit balance.
+        // Make payment for outstanding credit card balance.
         // creditCard.getCreditBalance() - amount;
         double creditBalance = creditCard.getCreditBalance();
-        if(amount <= creditBalance){
-            if (amount > balance){
-                System.out.println("There is insufficient balance in account to pay amount of " + amount);
-                System.out.println("Current Account balance: $" + balance);
-                return;
-            }
+        if (amount > creditBalance){
+            System.out.println("Payment amount of $" + amount + " is higher than owed balance in credit card.");
+            System.out.println("Current Credit Balance: $" + creditBalance);
+        }
+        else if (amount > balance){
+            System.out.println("There is insufficient balance in account to pay amount of " + amount);
+            System.out.println("Current Account Balance: $" + balance);
+        }
+        else {
             balance -= amount;
             creditBalance -= amount;
             creditCard.setCreditBalance(creditBalance);
-            System.out.println("Payment of $" + amount + ", Successful!");
-            System.out.println("Current Credit Balance: " + creditBalance);
-            System.out.println("Current Account Balance: " + balance);
-        }
-        else {
-            System.out.println("Payment amount of $" + amount + " is higher than owed balance in credit card.");
+            System.out.println("Payment of $" + amount + ", successful!");
             System.out.println("Current Credit Balance: $" + creditBalance);
+            System.out.println("Current Account Balance: $" + balance);
         }
 
     }
 
     public void printAccountDetails(){
         System.out.println("Account Number: " + accountNumber +
-                "\nAccount Type: " + accountType + "\nBalance: " + balance +
-                "\nBranch: " + branch + "\nCredit Card: " + creditCard +
-                "\nCustomer: " + customer + "\nInsurance: " + insurance +
-                "\nInterest Rate: " + interestRate + "\nTransfer Limit: " + transferLimit);
+                "\nAccount Type: " + accountType + "\nBalance: $" + balance +
+                "\nBranch: " + branch.getBranchName() + "\nCredit Card Number: " + creditCard.getCardNumber() +
+                "\nCustomer: " + customer.getCustomerName() + "\nInsurance: " + insurance.getPolicyName() +
+                "\nInterest Rate: " + interestRate + "%\nTransfer Limit: $" + transferLimit);
     }
 
     public void setAccountNumber(int number){
@@ -132,10 +164,10 @@ public class Account {
     public void transfer(Account transferAcc, double transferAmt){
         // Transfer to another account
         if(transferAmt > balance){
-            System.out.println("Account balance is insufficient for this transfer!");
+            System.out.println("Account balance $" + balance + " is insufficient for this transfer!");
         }
         else if(transferAmt > transferLimit){
-            System.out.println("Amount transferring exceeds account's transfer limit of " + transferAmt);
+            System.out.println("Amount to transfer exceeds account's transfer limit of " + transferAmt);
         }
         else {
             balance -= transferAmt;
@@ -149,15 +181,15 @@ public class Account {
     }
 
     public void withdraw(int withdrawAmt){
-        // Withdraw to cash
+        // Withdraw to cash from specified branchCode
         if(balance >= withdrawAmt)
         {
             balance -= withdrawAmt;
-            System.out.println("Amount Withdrawn: " + withdrawAmt);
-            System.out.println("Current Balance: " + balance);
+            System.out.println("Amount Withdrawn: $" + withdrawAmt);
+            System.out.println("Current Balance: $" + balance);
         }
         else{
-            System.out.println("Your bank balance is insufficient for this withdrawal!");
+            System.out.println("Bank balance of $" + balance + " is insufficient for this withdrawal!");
         }
 
     }
