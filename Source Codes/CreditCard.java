2b46610032cd595eac524bd77f8953d8d3dc1d3f
Lim Kuan Yong <66933839+Silversail4@users.noreply.github.com>
2024-03-03 16:57:18 +0800

    Update to print CSV value

--- Source Codes/CreditCard.java
+++ Source Codes/CreditCard.java
@@ -2,6 +2,7 @@ package CreditCard;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.time.format.DateTimeFormatter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -10,14 +11,15 @@ import java.util.List;
 
 public class CreditCard {
     private double balance = 0, creditLimit;
-    private String cardHolderName, cardNumber;
-    private Integer accountID;
+    private String customerName;
+    private int accountID;
+    private long cardNumber;
     private LocalDate expiryDate;
     private List<String> transactionHistory;
     private ArrayList<ArrayList<String>> creditDetails;
 
-    public CreditCard(String cardHolderName, Integer accountID, String cardNumber, double creditLimit){
-        this.cardHolderName = cardHolderName;
+    public CreditCard(String customerName, int accountID, long cardNumber, double creditLimit){
+        this.customerName = customerName;
         this.accountID = accountID;
         this.cardNumber = cardNumber;
         LocalDate currentDate = LocalDate.now(); // LocalDate is to retrieve year-month-day
@@ -27,10 +29,11 @@ public class CreditCard {
     }
 
 
-    public CreditCard(String filePath) {
+    public CreditCard(long cardNumber) {
+        this.cardNumber = cardNumber;
         creditDetails = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            BufferedReader reader = new BufferedReader(new FileReader("CreditCard/CreditCard.csv"));
             String line;
             boolean firstLine = true; // Flag to skip the first line
             while ((line = reader.readLine()) != null) {
@@ -43,20 +46,31 @@ public class CreditCard {
                 for (String part : parts) {
                     creditCard.add(part.trim());
                 }
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                expiryDate = LocalDate.parse(creditCard.get(3), formatter);
+
                 creditDetails.add(creditCard);
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        for (ArrayList<String> creditCard : creditDetails){
+            if (Long.parseLong(creditCard.get(0)) == cardNumber){
+                setCardNumber(Long.parseLong(creditCard.get(0)));
+                setCustomerName(creditCard.get(1));
+                setCreditLimit(Double.parseDouble(creditCard.get(2)));
+                setCardExpiry(LocalDate.parse(creditCard.get(3)));
+                setAccountNumber(Integer.parseInt(creditCard.get(4)));
+            }
+        }
     }
 
-    /* Testing Purposes
+    //Testing Purposes
     public static void main(String[] args){
-
+        CreditCard creditCard = new CreditCard(3108398698038530L);
+        creditCard.printCreditCardDetails();
     }
-
-     */
 
 
     public void chargeCredit(double amount){
@@ -101,15 +115,15 @@ public class CreditCard {
         return expiryDate;
     }
 
-    public String getCardHolderName(){
-        return cardHolderName;
+    public String getCustomerName(){
+        return customerName;
     }
 
     public double getCreditLimit(){
         return creditLimit;
     }
 
-    public String getCardNumber(){
+    public double getCardNumber(){
         return cardNumber;
     }
 
@@ -121,15 +135,15 @@ public class CreditCard {
         this.expiryDate = date;
     }
 
-    public void setCardHolderName(String name){
-        this.cardHolderName = name;
+    public void setCustomerName(String name){
+        this.customerName = name;
     }
 
     public void setCreditLimit(double limit){
         this.creditLimit = limit;
     }
 
-    public void setCardNumber(String number){
+    public void setCardNumber(long number){
         this.cardNumber = number;
     }
 
@@ -138,9 +152,8 @@ public class CreditCard {
     }
 
     public void printCreditCardDetails(){
-        System.out.println("Balance: " + balance
-                + "\nCard Holder Name: " + cardHolderName
-                + "\nCard Number: " + cardNumber
+        System.out.println("Card Number: " + cardNumber
+                + "\nCustomer Name: " + customerName
                 + "\nCredit Limit: " + creditLimit
                 + "\nExpiry Date: " + expiryDate
                 + "\nAccount ID: " + accountID);
