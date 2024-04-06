30209ea895ff8d6270355ce85b7409b2a65f692a
kwanny2301 <161841975+kwanny2301@users.noreply.github.com>
2024-04-06 20:53:44 +0800

    fix error

--- src/account/Customer.java
+++ src/account/Customer.java
@@ -123,7 +123,8 @@ public class Customer {
             BigDecimal interest = BigDecimal.valueOf(interestRate);
             
             Loan newLoan = Loan.applyForLoan(loans, principal, interest, termInMonths, credit);  // Use the credit object
-    
+            this.reviewAndProcessLoan(newLoan);
+
             if (newLoan != null) {
                 //loan already added to the list in the loan.applyForLoan method, so inside customer not required
                 System.out.println("Loan applied successfully.");
